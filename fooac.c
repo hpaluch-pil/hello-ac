d84721e97133c6627ff922261215b95cd8ac9cee
Henryk Paluch <henryk.paluch@pickering.cz>
2019-03-06 07:07:12 +0000

    Release 1.2 - new function - API backward compatible

--- fooac.c
+++ fooac.c
@@ -6,8 +6,13 @@
 #include <stdlib.h>
 
 void fooac_func1(int p1){
-	printf("Hello from %s:%u\n", __FILE__, __LINE__);
+	printf("Hello from %s:%u function: %s()\n", __FILE__, __LINE__,__FUNCTION__);
 	printf("Parameter p1=%d\n",p1);
 	printf("New function fix (full compatible)\n");
 }
 
+void fooac_func2(int p1){
+	printf("Hello from %s:%u function: %s()\n", __FILE__, __LINE__,__FUNCTION__);
+	printf("Parameter p1=%d\n",p1);
+	printf("This is new function (API only backward compatible)\n");
+}
--- helloac.c
+++ helloac.c
@@ -11,6 +11,7 @@ int main(int argc, char **argv){
 	printf("Hello from %s:%u\n", __FILE__, __LINE__);
 
 	fooac_func1(123);
+	fooac_func2(456789);
 
 	return EXIT_SUCCESS;
 }
