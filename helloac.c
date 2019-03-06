305fdb3c234a72e3c0187d10e9462b821815ac68
Henryk Paluch <henryk.paluch@pickering.cz>
2019-03-06 07:33:11 +0000

    Print program name/version, added empty lines

--- helloac.c
+++ helloac.c
@@ -9,8 +9,10 @@
 int main(int argc, char **argv){
 
 	printf("Hello from %s:%u\n", __FILE__, __LINE__);
+	printf("Program '%s' version: '%s'\n\n", PACKAGE, VERSION);
 
 	fooac_func1(123);
+	puts("");
 	fooac_func2(456789);
 
 	return EXIT_SUCCESS;
